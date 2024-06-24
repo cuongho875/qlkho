6e220d4c0571217f93061d67087c7e62492f92ea
Somed co ltd <cuongho875@gmail.com>
2024-06-24 15:56:46 +0700

    push code

--- backend/src/controllers/inventory.ts
+++ backend/src/controllers/inventory.ts
@@ -34,5 +34,36 @@ const importInventory = async (req:Request,res: Response,next:NextFunction)=>{
         res.status(404).json('not found')
     }
 }
+const exportInventory = async(req:Request,res: Response,next:NextFunction)=>{
+    const id = req.params.id;
+    const currentProduct = (await prisma.product.findUnique({
+        where:{
+            productId:parseInt(id)
+        },
+        select:{
+            quantity:true
+        }
+    }))
+    if(currentProduct){
+        const quantity = req.body.quantity;
+        const product = await prisma.product.update({
+        where:{
+            productId:parseInt(id)
+        },
+        data:{
+            quantity:currentProduct.quantity-parseInt(quantity),
+            exportWarehouses:{
+                create:[{
+                    quantity:parseInt(quantity),
+                }]
+            }
+        }
+        })
+        res.json(product)
+    }
+    else{
+        res.status(404).json('not found')
+    }
+}
 
-export {importInventory}
+export {importInventory, exportInventory}
--- backend/src/routes/api/inventory.ts
+++ backend/src/routes/api/inventory.ts
@@ -1,7 +1,8 @@
 import { Router } from "express";
-import { importInventory } from "~/controllers/inventory";
+import { exportInventory, importInventory } from "~/controllers/inventory";
 
 
 const inventoryRoute = Router()
 inventoryRoute.post('/importInventory/:id',importInventory)
+inventoryRoute.post('/exportInventory/:id',exportInventory)
 export default inventoryRoute
